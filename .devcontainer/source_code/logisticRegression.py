5824843def25ea1e85945acea6f0c5748ef5116d
usmankhalid-95 <ukhalid616@gmail.com>
2024-10-08 20:55:28 +0700

    unit tested

--- .devcontainer/source_code/logisticRegression.py
+++ .devcontainer/source_code/logisticRegression.py
@@ -211,3 +211,22 @@ class Ridge(LogisticRegression):
 class Normal(LogisticRegression):
     def __init__(self, k, n, method, alpha=0.001, max_iter=5000):
         super().__init__(regularization=None, k=k, n=n, method=method, alpha=alpha, max_iter=max_iter)
+
+class RidgePenalty():
+    def __init__(self, l):
+        self.l = l
+
+    def __call__(self, theta):
+        return self.l * np.sum(np.square(theta))
+
+    def derivation(self, theta):
+       return self.l * 2 * theta
+
+class Ridge():
+    def __init__(self, l, k, n, method, alpha=0.001, max_iter=5000):
+        regularization = RidgePenalty(l)
+        super().__init__(regularization, k, n, method, alpha, max_iter)
+
+class Normal():
+    def __init__(self, k, n, method, alpha=0.001, max_iter=5000):
+        super().__init__(regularization=None, k=k, n=n, method=method, alpha=alpha, max_iter=max_iter)
